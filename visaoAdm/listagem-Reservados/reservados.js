6c77c8a48f61fb06d31e03aa5a47ad24c1278e15
LUÍS OTÁVIO COELHO FERREIRA <54150243875@info.local>
2026-09-16 17:24:35 -0300

    sparkles: Mudando status de retirada em JS

---
+++ visaoAdm/listagem-Reservados/reservados.js
@@ -0,0 +1,26 @@
+
+const checkboxes = document.querySelectorAll(`input[type= "checkbox"]`);
+const statusRetirada =document.querySelectorAll(".status");
+
+checkboxes.forEach((caixinha , indice, lista) =>{
+
+    //corpo da função 
+    console.log("caixinha: ", caixinha);
+    console.log("Indice: ", indice);
+    console.log("lista: ", lista);
+
+    caixinha.addEventListener("change", () => {
+
+        if(caixinha.checked == true){
+            statusRetirada[indice].classList.remove("aguardando");
+            statusRetirada[indice].classList.add("retirado");
+            statusRetirada[indice].textContent = "retirado";
+        }
+        else{
+            statusRetirada[indice].classList.remove("retirado");
+            statusRetirada[indice].classList.add("aguardando");
+            statusRetirada[indice].textContent = "Aguardando retirada";
+        }
+    })
+    
+}) 
